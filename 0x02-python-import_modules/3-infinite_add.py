32b55b816fb39d57bffd10e356a32174e049d239
MichaelDecent <michaeldecent2@gmail.com>
2022-12-01 19:22:29 +0000

    a program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    lent = len(argv)
+    if lent == 1:
+        print ("{}".format(lent - 1))
+    else:
+        i = 1
+        result = 0
+        while i < lent:
+            result += int(argv[i])
+            i += 1
+        print("{}".format(result))
