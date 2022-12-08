edd1f1906839ca318ab75e1eed1a4e77b643013f
MichaelDecent <michaeldecent2@gmail.com>
2022-12-08 19:38:41 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary, key=lambda k: a_dicti onary[k]) \
-        if a_dictionary else None
+    return max(a_dictionary, key=lambda k: a_dicti onary[k])\
+    if a_dictionary else None
