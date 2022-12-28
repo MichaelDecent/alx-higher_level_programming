813824f0cd2b705d40229d6e5c3ad2cf7e272a6c
MichaelDecent <michaeldecent2@gmail.com>
2022-12-28 07:18:07 +0000

    a class Square that defines a square by

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,10 @@ class Square:
         Args:
             size(int): size of the square
         """
-        if size < 0:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif type(size) is not int:
+        elif size < 0:
+
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
