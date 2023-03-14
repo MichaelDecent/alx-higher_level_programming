11119833ca2c7b9a294c791dc586dc057d1435bb
MichaelDecent <michaeldecent2@gmail.com>
2023-03-14 15:03:29 +0000

    a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 /**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: a pointer to the singly linked list
  *
@@ -7,20 +29,43 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp, *prev;
+	listint_t *mid1, *mid2;
+	listint_t *fwd, *bck;
+	int len, i;
 
 	if (*head == NULL)
 		return (1);
-	temp = (*head)->next;
-	prev = *head;
-
-	while (temp != NULL)
+	bck = *head;
+	len = 0;
+	while (fwd != NULL)
 	{
-		if (prev->n == temp->n)
-			return (1);
-		prev = temp;
-		temp = prev->next;
+		fwd = bck->next;
+		bck = fwd;
+		len++;
 	}
-	return (0);
+	mid1 = *head;
+	mid2 = (*head)->next;
+	i = 1;
 
+	while (i < (len / 2))
+	{
+		mid1 = mid2;
+		mid2 = mid1->next;
+		i++;
+	}
+
+	bck = reverse_listint(&mid2);
+	mid2 = bck;
+	mid1 = *head;
+
+	while (mid1 != NULL && mid2 != NULL)
+	{
+		if (mid1->n != mid2->n)
+			return (0);
+		bck = mid1->next;
+		mid1 = bck;
+		fwd = mid2->next;
+		mid2 = fwd;
+	}
+	return (1);
 }
