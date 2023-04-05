70d78496e93a271df3723d27dc79faddba95dd8a
MichaelDecent <michaeldecent2@gmail.com>
2023-04-05 05:56:48 +0000

    updated Test Cases

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,10 +7,16 @@ def text_indentation(text):
         Args:
             text(str): text to be broken into smaller strings
     """
-    delimiters = ".:?"
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     else:
-        text_string = re.split("[" + re.escape(delimiters) + "]", text)
-        for text_str in text_string:
-            print(text_str, '\n')
+        text_1 = text.replace(".", ".\n\n")
+        text_2 = text_1.replace("?", "?\n\n")
+        text_3 = text_2.replace(":", ":\n\n")
+        text_string = text_3.split("\n")
+        for i in range(len(text_string)):
+            text_string[i] = text_string[i].strip()
+        print("\n".join(text_string), end="")
+
+            
+        
