9769dfa269338d2fdb75e0cdc92cab37f0de85d5
MichaelDecent <michaeldecent2@gmail.com>
2023-04-11 17:44:35 +0000

    Checks for Exact same object

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@
 class MyList(list):
     """ class MyList that inherits from list """
     def print_sorted(self):
-        """ A method that prints the list, 
+        """ A method that prints the list,
             but sorted (ascending sort)
         """
         print(sorted(self))
