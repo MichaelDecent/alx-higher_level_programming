9bb1b6f9417410c3030ba728193f4ec927bf0d3f
MichaelDecent <michaeldecent2@gmail.com>
2023-04-14 09:23:54 +0000

    Solution to Task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,11 +9,8 @@ def append_after(filename="", search_string="", new_string=""):
         search_strings = string to replace
         new_string = the new string
     """
-    new_text = ""
-    with open(filename) as fp:
+    with open(filename, 'a+') as fp:
         for line in fp:
-            new_text += line
             if search_string in line:
-                new_text += new_string
-    with open(filename, "w") as fp:
-        fp.write(new_text)
+                fp.write('\n')
+                fp.write(new_text)
