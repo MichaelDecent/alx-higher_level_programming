42768a37958d9b6a5db301a9c62c55c20f8db6fa
MichaelDecent <michaeldecent2@gmail.com>
2023-04-14 09:21:40 +0000

    Solution to Task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a text file insertion function"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Inserts text after each line containing a given string in a file
         Args:
