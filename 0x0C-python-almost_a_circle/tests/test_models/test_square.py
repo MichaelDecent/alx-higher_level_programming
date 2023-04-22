4864fd06d66683537013ba9fbd31c0816c8a81ed
MichaelDecent <michaeldecent2@gmail.com>
2023-04-22 01:52:48 +0000

    Update Test Cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,18 +2,21 @@
 
 """
 This module contains test cases based on Square class
+
 The tests can be run using these commands:
     python3 -m unittest discover tests
-    python3 -m unittest tests/test_models/test_rectangle.py
+    python3 -m unittest tests/test_models/test_square.py
+
 Below are several test cases on the class
 """
 
 import unittest
-import os
 import pep8
 from io import StringIO
-from models.square import Square
+from models import square
 from contextlib import redirect_stdout
+
+Square = square.Square
 
 
 class TestPep8(unittest.TestCase):
@@ -25,60 +28,140 @@ class TestPep8(unittest.TestCase):
         num_err += style.check_files(files).total_errors
         self.assertEqual(num_err, 0, 'Wrong Pep8 style, adjust your code !')
 
+
+class TestSquare(unittest.TestCase):
+    """Several test cases for the square class"""
+
     def setUp(self):
         """For dictionary conversion"""
-        self.square = Square(10, 2, 1, 5)
+        self.r_dict = Square(2, 3, 4, 5)
 
-    def test_square_attr(self):
-        """ passing size attr """
-        a = Square(1)
-        self.assertEqual(a.size, 1)
+    def test_orderly_inpt_val(self):
+        """Test input if correctly in order"""
+        r1 = Square(3, 2, 60, 10)
 
-    def test_attrs(self):
-        """ passing 2 attrs """
-        a = Square(1, 2)
-        self.assertEqual(a.x, 2)
-        self.assertEqual(a.size, 1)
+        self.assertEqual(r1.width, 3)
+        self.assertEqual(r1.height, 3)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 60)
+        self.assertEqual(r1.id, 10)
 
-    def test_more_attr(self):
-        """ paasing 3 attrs """
-        b = Square(10, 2, 3)
-        self.assertEqual(b.x, 2)
-        self.assertEqual(b.y, 3)
-        self.assertEqual(b.size, 10)
+    def test_default(self):
+        """Test default"""
+        r2 = Square(1)
 
-    def test_excess_attr(self):
-        """ Excess attrs """
-        with self.assertRaises(TypeError):
-            Square(1, 2, 3, 4, 5)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 1)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
 
-    def test_wrong_attr(self):
-        """ passing a wrong type of attribute """
+    def test_default_val(self):
+        """Test default values"""
+        r2 = Square(5, 0, 0, 3)
+
+        self.assertEqual(r2.width, 5)
+        self.assertEqual(r2.height, 5)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.id, 3)
+
+    def test_wrong_input(self):
+        """Test invalid inputs"""
+
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square("1")
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            Square(1, "2")
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(1, 2, "3")
+            Square([2, 3], 3, 5, 3)
+            Square("20", 3, 3, 5)
 
-    def test_less_than_zero(self):
-        """ passing attr less than zero """
         with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Square(-18, 4, 5, 3)
+            Square(0, 5, 6, 3)
+            Square(-1)
             Square(0)
-        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
-            Square(1, -2)
-        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-            Square(1, 2, -3)
 
-    def test_display(self):
-        """ testing the magic method __str__ """
-        with StringIO() as buf, redirect_stdout(buf):
-            print(self.square, end='')
-            output = buf.getvalue()
-        self.assertEqual(output, "[Square] (5) 2/1 - 10")
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(9, "4")
+            Square(3, (3,), 4, 6)
+
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Square(2, -4, 5, 6)
+            Square(6, 0, 6, 3)
+
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(3, 4, (3, 4))
+
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Square(2, 4, -5, 6)
+
+    def test_priv_attr(self):
+        """Test if attributes are private"""
+
+        with self.assertRaises(AttributeError):
+            print(Square.width)
+            print(Square.height)
+            print(Square.x)
+            print(Square.y)
+            print(Square.__width)
+            print(Square.__height)
+            print(Square.__x)
+            print(Square.__y)
+
+    def test_over_under_args(self):
+        """Test for over and under arguments supplied"""
+
+        with self.assertRaises(TypeError):
+            Square(1, 2, 3, 4, 5, 2, 3, 5, 6, 7)
+            Square()
+            Square(1)
 
     def test_to_dictionary(self):
-        """ testing to dictionary function """
-        sq_dic = self.square.to_dictionary()
-        self.assertEqual(sq_dic, {'size': 10, 'x': 2, 'y': 1, 'id': 5})
-        self.assertEqual(type(sq_dic), dict)
+        """Test to confirm dictionary"""
+
+        dic = {"size": 2, "x": 3, "y": 4, "id": 5}
+        my_type = self.r_dict.to_dictionary()
+        self.assertEqual(my_type, dic)
+        self.assertTrue(isinstance(my_type, dict))
+
+    def test_obj_to_str(self):
+        """Test object conversion to strig"""
+
+        a = str(self.r_dict)
+        strng = "[Square] (5) 3/4 - 2"
+        self.assertEqual(a, strng)
+        self.assertEqual(type(a), str)
+
+    def test_display(self):
+        """Check if stdout display is equal"""
+
+        check = "\n\n\n\n   ##\n   ##\n"
+        with StringIO() as buf, redirect_stdout(buf):
+            self.r_dict.display()
+            output = buf.getvalue()
+        self.assertEqual(output, check)
+
+        check2 = "\n\n\n    ####\n    ####\n    ####\n    ####\n"
+        r4 = Square(4, 4, 3, 3)
+        with StringIO() as buff, redirect_stdout(buff):
+            r4.display()
+            output_2 = buff.getvalue()
+        self.assertEqual(output_2, check2)
+
+    def test_area(self):
+        """Check if area is correct"""
+        self.assertEqual(self.r_dict.area(), 4)
+
+    def test_update(self):
+        """Test for updated attributes"""
+        self.r_dict.update(3)
+        self.assertEqual(str(self.r_dict), "[Square] (3) 3/4 - 2")
+        self.r_dict.update(3, 9)
+        self.assertEqual(str(self.r_dict), "[Square] (3) 3/4 - 9")
+        self.r_dict.update(3, 9, 8)
+        self.assertEqual(str(self.r_dict), "[Square] (3) 8/4 - 9")
+        self.r_dict.update(3, 9, 8, 11)
+        self.assertEqual(str(self.r_dict), "[Square] (3) 8/11 - 9")
+
+        """Test for **kwargs"""
+        self.r_dict.update(size=33, x=209, id=10)
+        self.assertEqual(str(self.r_dict), "[Square] (10) 209/11 - 33")
+        self.r_dict.update(x=303, y=400)
+        self.assertEqual(str(self.r_dict), "[Square] (10) 303/400 - 33")
