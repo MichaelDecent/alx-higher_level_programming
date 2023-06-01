53f63f5027047b94d8786d725355723243f02407
MichaelDecent <111002205+MichaelDecent@users.noreply.github.com>
2023-06-01 04:05:25 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" A Module that reads a file """
-
-
-def read_file(filename=""):
-    """  a function that reads a text file (UTF8) and prints it to stdout """
-    with open(filename, 'r', encoding="utf-8") as fp:
-        print(fp.read(), end='')
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" A module that defines a class to JSON function """
-
-
-def class_to_json(obj):
-    """ it returns a ictionary description or
-        JSON serialization of an object
-    """
-    return (obj.__dict__)
