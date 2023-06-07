64b5714c9f888ff6a0d819511c3242a831fa0d3e
MichaelDecent <michaeldecent2@gmail.com>
2023-06-07 12:42:35 +0000

    updating solution to the project

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,13 +11,17 @@ def matrix_divided(matrix, div):
              a new matrix
     """
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a\
+                matrix (list of lists) of integers/floats")
     if not all([(type(row) is list) for row in matrix]):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all([all([(type(ele) is int or type(ele) is float) for ele in row]) for row in matrix]):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a\
+                matrix (list of lists) of integers/floats")
+    if not all([all([(type(ele) is int or type
+                (ele) is float) for ele in row]) for row in matrix]):
+        raise TypeError("matrix must be a matrix\
+                (list of lists) of integers/floats")
     if not all([len(row) == len(matrix[1]) for row in matrix]):
-         raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError("Each row of the matrix must have the same size")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -25,4 +29,3 @@ def matrix_divided(matrix, div):
 
     else:
         return [[round(ele / div, 2) for ele in row] for row in matrix]
-
