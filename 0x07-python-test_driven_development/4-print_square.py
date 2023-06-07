342842c1f16b4961f7407fe0ea5c8c957a153ade
MichaelDecent <michaeldecent2@gmail.com>
2023-06-07 13:06:24 +0000

    update on Python - Test-driven development project

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,9 +7,10 @@ def print_square(size):
         Args:
             size(int): size of the Square
     """
-    if type(size) is not int and type(size) is not float:
+    if type(size) is int:
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        for row in range(size):
+            print("#" * size)
+    else:
         raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    for row in range(size):
-        print("#" * size)
