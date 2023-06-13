ec13b84db42e463cb420608163bba93cacff17fd
MichaelDecent <michaeldecent2@gmail.com>
2023-06-13 11:38:10 +0000

    update on python-inheritance project

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,5 +11,5 @@ class BaseGeometry:
         """ validates value """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
