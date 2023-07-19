fb341655c71cd7f41d7fe0265f7627083fa84b39
MichaelDecent <michaeldecent2@gmail.com>
2023-07-19 21:00:46 +0000

    update on task 0-2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,7 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- grants SELECT privilage to user_0d_2 on database hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
