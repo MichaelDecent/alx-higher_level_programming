2e1bdf9e2adb81ca31bff0a6af1ff1ba0ec348b8
MichaelDecent <michaeldecent2@gmail.com>
2023-07-20 00:57:48 +0000

    update on task 3-9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,4 +4,4 @@ SELECT name, id FROM cities
 WHERE state_id = (
                     SELECT id FROM states 
                     WHERE name = 'California')
-ORDER BY cities.id
+ORDER BY cities.id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,3 +5,4 @@
 SELECT cities.id, cities.name, states.name
 FROM cities
 NATURAL JOIN states
+ORDER BY cities.id ASC;
