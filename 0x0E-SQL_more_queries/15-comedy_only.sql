764037558821872664da6793c5213bfa600d9c17
MichaelDecent <michaeldecent2@gmail.com>
2023-07-20 17:48:03 +0000

    update on task 14-16

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,15 +1,11 @@
---  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
--- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
--- First column must be called genre
--- Second column must be called number_of_shows
--- Don’t display a genre that doesn’t have any shows linked
--- Results must be sorted in descending order by the number of shows linked
--- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+-- Each record displays: tv_shows.title
+-- Results are sorted in ascending order by the show title
 
-SELECT name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id = (SELECT tv_genres.id
+                                FROM tv_genres
+                                WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title ASC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,15 +1,9 @@
---  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
--- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
--- First column must be called genre
--- Second column must be called number_of_shows
--- Don’t display a genre that doesn’t have any shows linked
--- Results must be sorted in descending order by the number of shows linked
--- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- A script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- Each record displays: tv_shows.title - tv_genres.name
+-- Results are sorted in ascending order by the show title and genre name
 
-SELECT name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
