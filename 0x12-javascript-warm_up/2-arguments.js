90242eeb327b548d89c82e01ca818bb3aa874d2c
MichaelDecent <michaeldecent2@gmail.com>
2023-08-14 23:19:36 +0000

    added task 0 - 6

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 if (process.argv.length === 1) { console.log('Argument found'); }
-if (process.argv.length > 1) { console.log('Arguments found'); } else { console.log('No argument'); }
+else if (process.argv.length > 1) { console.log('Arguments found'); } else { console.log('No argument'); }
