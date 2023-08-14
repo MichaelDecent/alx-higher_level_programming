d612156b0e89548c3dd1daa5a76152df4ce38ffb
MichaelDecent <michaeldecent2@gmail.com>
2023-08-14 23:11:06 +0000

    added task 0 - 6

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-if (process.argv.length > 1) {
+if (process.argv[2]) {
   for (let i = 2; process.argv[i]; i++) { console.log(process.argv[i]); }
 } else { console.log('No argument'); }
