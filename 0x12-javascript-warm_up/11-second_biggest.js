b79c5ff1d6937cd210cdccbc7e8e19bf0f2c79d2
MichaelDecent <michaeldecent2@gmail.com>
2023-08-15 19:17:36 +0000

    upadate on task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,31 +5,10 @@ function secondBiggest (array) {
   if (!myArr || myArr.length === 1) {
     return 0;
   } else {
-    let max = Number(myArr[0]);
-    let max2 = Number(myArr[0]);
-    for (let i = 0; myArr[i]; i++) 
-    {
-      console.log(`max = ${max} before`)
-      console.log(`next item = ${myArr[i + 1]}`)
-
-      if (Number(myArr[i + 1]) > max) 
-      {
-        max = Number(myArr[i + 1]);
-      }
-      console.log(`max = ${max} after`)
-      console.log(`current item = ${myArr[i]}`)
-      console.log(`max2 =  ${max2} before`)
-
-      if (Number(myArr[i]) > max2 && Number(myArr[i]) < max) 
-      {
-          max2 = Number(myArr[i]);
-      }
-      console.log(`max2 =  ${max2} after`)
-      console.log('--------------')
-
-    }
-    return max2;
+    console.log(myArr.sort((a, b) => a - b));
+    return (myArr[myArr.length - 2])
   }
 }
+
 const ans = secondBiggest(myArr);
 console.log(ans);
