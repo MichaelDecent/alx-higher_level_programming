2ce02e5ae455293588e71a682391807b9ef4497d
MichaelDecent <michaeldecent2@gmail.com>
2023-08-16 11:56:50 +0000

    update on task 5

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Square1 = require('./5-square');
 
 class Square extends Square1 {
   charPrint (c) {
-    if (c != null) {
+    if (c) {
       for (let i = 0; i < this.height; i++) {
         let output = '';
         for (let j = 0; j < this.width; j++) {
