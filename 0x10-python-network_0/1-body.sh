155b8662b6f0b4d48a9adf27b355aeb56ec3b330
MichaelDecent <michaeldecent2@gmail.com>
2023-08-31 21:03:01 +0000

    updated task 0 - 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL -X "GET" $1
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sI $1 | grep -i "allow" | sed "s/allow: //I"
